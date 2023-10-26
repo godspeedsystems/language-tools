79a45ea4837459f028c0524d88ed4134f9e37a12
rohitgodspeed <rohit@godspeed.systems>
2023-10-26 17:58:21 +0530

    Merge pull request #26 from godspeedsystems/autoComplete1.1
    Auto complete1.1

---
+++ server/src/server.ts
@@ -0,0 +1,142 @@
+import {
+  createConnection,
+  DidChangeConfigurationNotification,
+  InitializeParams,
+  InitializeResult,
+  ProposedFeatures,
+  TextDocuments,
+  TextDocumentSyncKind,
+} from "vscode-languageserver/node";
+import { TextDocument } from "vscode-languageserver-textdocument";
+
+// Create a simple document
+let documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
+
+let hasConfigurationCapability = false;
+let hasWorkspaceFolderCapability = false;
+let hasDiagnosticRelatedInformationCapability = false;
+
+const connection = createConnection(ProposedFeatures.all);
+
+connection.onInitialize((params: InitializeParams) => {
+  const capabilities = params.capabilities;
+
+  // Does the client support the `workspace/configuration` request?
+  // If not, we fall back using global settings.
+  hasConfigurationCapability = !!(
+    capabilities.workspace && !!capabilities.workspace.configuration
+  );
+
+  hasWorkspaceFolderCapability = !!(
+    capabilities.workspace && !!capabilities.workspace.workspaceFolders
+  );
+
+  hasDiagnosticRelatedInformationCapability = !!(
+    capabilities.textDocument &&
+    capabilities.textDocument.publishDiagnostics &&
+    capabilities.textDocument.publishDiagnostics.relatedInformation
+  );
+
+  const result: InitializeResult = {
+    capabilities: {
+      textDocumentSync: TextDocumentSyncKind.Incremental,
+      // Tell the client that this server supports code completion.
+      completionProvider: {
+        resolveProvider: true,
+      },
+    },
+  };
+
+  if (hasWorkspaceFolderCapability) {
+    result.capabilities.workspace = {
+      workspaceFolders: {
+        supported: true,
+      },
+    };
+  }
+
+  return result;
+});
+
+connection.onInitialized(() => {
+  if (hasConfigurationCapability) {
+    debugger;
+    connection.client.register(
+      DidChangeConfigurationNotification.type,
+      undefined
+    );
+  }
+
+  if (hasWorkspaceFolderCapability) {
+    connection.workspace.onDidChangeWorkspaceFolders((_event) => {
+      connection.console.log("Workspace folder change event received.");
+    });
+  }
+});
+
+// Example settings
+type ExampleSettings = {
+  maxNumberOfProblems: number;
+};
+
+const defaultSettings: ExampleSettings = { maxNumberOfProblems: 100 };
+let globalSettings: ExampleSettings = defaultSettings;
+
+// Cache the settings of all the open documents
+const documentSettings: Map<string, Thenable<ExampleSettings>> = new Map();
+
+connection.onDidChangeConfiguration((change) => {
+  if (hasConfigurationCapability) {
+    // Reset all cached defaultSettings
+    documentSettings.clear();
+  } else {
+    globalSettings = <ExampleSettings>(
+      (change.settings.languageServerExample || defaultSettings)
+    );
+  }
+
+  // Revalidate all open text documents
+});
+
+function getDocumentSettings(resource: string): Thenable<ExampleSettings> {
+  if (!hasConfigurationCapability) {
+    return Promise.resolve(globalSettings);
+  }
+
+  let result = documentSettings.get(resource);
+
+  if (!result) {
+    result = connection.workspace.getConfiguration({
+      scopeUri: resource,
+      section: "godspeed-language-server",
+    });
+
+    documentSettings.set(resource, result);
+  }
+
+  return result;
+}
+
+// Only keep the settings for open documents
+documents.onDidClose((event) => {
+  console.log("Document closed.");
+  documentSettings.delete(event.document.uri);
+});
+
+// This action is emitted when the document is first opened
+// or there is a change in the document
+documents.onDidChangeContent((change) => {
+  console.log("Document content changed.");
+  validateDocument(change.document);
+});
+
+async function validateDocument(textDocument: TextDocument): Promise<void> {
+  // In this simple example we get the settings for every validate run.
+  let settings = await getDocumentSettings(textDocument.uri);
+  console.log({ settings });
+  return Promise.resolve();
+}
+
+documents.listen(connection);
+
+connection.listen();
